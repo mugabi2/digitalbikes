923c1009053ad52bc30eabc24401c0019ec62141
samuel <samuelmugabi2@gmail.com>
2019-08-27 10:16:29 +0300

    Notifications working in background

---
+++ app/src/main/java/com/stardigitalbikes/samuelhimself/bible1/myFirebaseInstanceIdService.java
@@ -0,0 +1,6 @@
+package com.stardigitalbikes.samuelhimself.bible1;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class myFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
